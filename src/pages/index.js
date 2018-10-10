e21cbda156802474fc180b1e6c7b087f914fc010
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-10-10 12:14:44 -0500

    modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import ColorSwatch from '../re/ColorSwatch.bs'
 import AvatarGenerator from '../re/AvatarGenerator.bs'
 import IconLink from '../re/IconLink.bs'
+import Modal from '../re/Modal.bs'
 
 import Layout from '../components/layout'
 
@@ -27,45 +28,54 @@ const Wordmark = () => (
   </a>
 )
 
-const IndexPage = () => (
-  <Layout>
-    <div className="body-bg-left" />
-    <div className="body-bg-right" />
-    <header className="Layout-header">
-      <div className="Layout-left">
-        <Wordmark />
-        <h1 className="Text-title">Persona</h1>
-        <span className="Text-subtitle">Avatar Generator</span>
-      </div>
-      <div className="Layout-right">
-        <h2 style={{ maxWidth: 320 }} className="Text-description">
-          A playful avatar generator for the modern age.
-        </h2>
-      </div>
-    </header>
-    <main className="Layout-main">
-      <AvatarGenerator />
-    </main>
-    <footer className="Layout-footer">
-      <div className="Layout-left">
-        <span className="Text-overline">Built by</span>
-        <br />
-        <Wordmark />
-      </div>
-      <div className="Layout-right">
-        <span className="Text-marketing mb-4 d-block">
-          Visually build robust native front-end apps with usable code.
-        </span>
-        <IconLink
-          className="Text-link"
-          href="https://draftbit.com"
-          title="Build fobust native front-end apps with usable code using Draftbit"
-          label="draftbit.com"
-          icon="arrow"
+export default class IndexPage extends React.PureComponent {
+  state = {
+    showModal: true,
+  }
+  render() {
+    return (
+      <Layout>
+        <div className="body-bg-left" />
+        <div className="body-bg-right" />
+        <header className="Layout-header">
+          <div className="Layout-left">
+            <Wordmark />
+            <h1 className="Text-title">Persona</h1>
+            <span className="Text-subtitle">Avatar Generator</span>
+          </div>
+          <div className="Layout-right">
+            <h2 style={{ maxWidth: 320 }} className="Text-description">
+              A playful avatar generator for the modern age.
+            </h2>
+          </div>
+        </header>
+        <main className="Layout-main">
+          <AvatarGenerator />
+        </main>
+        <footer className="Layout-footer">
+          <div className="Layout-left">
+            <span className="Text-overline">Built by</span>
+            <br />
+            <Wordmark />
+          </div>
+          <div className="Layout-right">
+            <span className="Text-marketing mb-4 d-block">
+              Visually build robust native front-end apps with usable code.
+            </span>
+            <IconLink
+              className="Text-link"
+              href="https://draftbit.com"
+              title="Build fobust native front-end apps with usable code using Draftbit"
+              label="draftbit.com"
+              icon="arrow"
+            />
+          </div>
+        </footer>
+        <Modal
+          visible={this.state.showModal}
+          onToggle={() => this.setState({ showModal: !this.state.showModal })}
         />
-      </div>
-    </footer>
-  </Layout>
-)
-
-export default IndexPage
+      </Layout>
+    )
+  }
+}
---
+++ src/re/Modal.re
@@ -0,0 +1,40 @@
+[%bs.raw {|require('./Modal.css')|}];
+let component = ReasonReact.statelessComponent("Modal");
+
+let make = (~visible, ~onToggle, _children) => {
+  ...component,
+  render: _self => switch(visible) {
+    | false => ReasonReact.null
+    | _ =>
+    <>
+    <div className="Modal-overlay" />
+    <div className="Modal-container">
+      <button onClick=(_ => onToggle()) className="Modal-close">
+       <img width="18" height="18" className="Modal-close--icon" src="/images/close.svg" />
+      </button>
+      <div className="Modal-avatar" />
+      <h3 className="Modal-title"> {ReasonReact.string("Enjoy your avatar!")} </h3>
+      <span className="Modal-subtitle">
+        {
+          ReasonReact.string(
+            "Share it with your friends and see what ridiculousness they come up with!",
+          )
+        }
+      </span>
+      <div className="Modal-buttons">
+        <a href="#" className="Modal-button Modal-button--twitter">
+          {ReasonReact.string("Twitter")}
+        </a>
+        <a href="#" className="Modal-button Modal-button--fb">
+          {ReasonReact.string("Facebook")}
+        </a>
+      </div>
+    </div>
+    </>
+  }
+};
+
+let default = ReasonReact.wrapReasonForJs(~component, jsProps => make(
+    ~visible=jsProps##visible,
+    ~onToggle=jsProps##onToggle,
+    [||]));
